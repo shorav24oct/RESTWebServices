65b3651aa2403585b244c5179f78e95fe0ef0f1f
shoravs <sharmasaurav142@gmail.com>
2022-09-12 08:17:56 +0530

    add model and service classes

--- messenger/src/main/java/com/sk/services/messenger/resources/MessageResource.java
+++ messenger/src/main/java/com/sk/services/messenger/resources/MessageResource.java
@@ -1,5 +1,10 @@
 package com.sk.services.messenger.resources;
 
+import java.util.List;
+
+import com.sk.services.messenger.model.Message;
+import com.sk.services.messenger.service.MessageService;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -7,11 +12,13 @@ import jakarta.ws.rs.core.MediaType;
 
 @Path("/messages")
 public class MessageResource {
+	
+	MessageService service = new MessageService();
 
 	@GET
-	@Produces(value = MediaType.TEXT_PLAIN)
-	public String getMessage() {
-		return "Hello World !";
+	@Produces(value = MediaType.APPLICATION_XML)
+	public List<Message> getMessage() {
+		return service.getAllMessages();
 	}
 
 }
